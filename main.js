7444d9dae2d01b0235ec998f7b43b61fc37968a5
박태원 <producer82@naver.com>
2020-04-19 02:51:10 +0900

    http and udp

--- main.js
+++ main.js
@@ -1,11 +1,56 @@
 const fs = require("fs");
 const bencode = require("bencode");
-const urlParse = require("url");
+const url = require("url");
 const dgram = require("dgram");
+const http = require("http");
 const client = dgram.createSocket("udp4");
 
-fs.readFile("test.torrent", (err, data) => {
-	
+const message = Buffer.from("hello", "utf-8");
+
+fs.readFile("music.torrent", (err, data) => {
+    torrent = bencode.decode(data);
+    tracker = url.parse(torrent.announce.toString('utf8'));
+
+    console.log(torrent.info.filehash.toString('hex'));
+
+    const options = {
+        hostname: tracker.hostname,
+        port: tracker.port,
+        path: '?' + tracker.path + '=' + torrent.info.filehash.toString('hex'),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': message.length
+        }
+    }
+
+    if (tracker.protocol == "http:") {
+        console.log("http protocol");
+        const req = http.request(options, res => {
+            console.log(`statusCode: ${res.statusCode}`);
+          
+            res.on('data', d => {
+                console.log(d);
+            });
+        });
+          
+        req.on('error', err => {
+            console.error(err);
+        });
+    }
+    else if (torrent.protocol = "udp:") {
+        console.log("udp protocol");
+        client.send(message, 0, message.length, tracker.port, tracker.hostname, (err, bytes) => {
+            if (err) throw err;
+        });
+
+        client.on('msg', (msg) => {
+            console.log(msg);
+        });
+    }
+    else {
+        console.log("알 수 없는 프로토콜 (Unknown Protocol");
+    }
 });
 
 
