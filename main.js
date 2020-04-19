53ac8678c47be1fe911feb13b31e779b90a1626b
박태원 <producer82@naver.com>
2020-04-20 01:33:34 +0900

    request info_hash to tracker

--- main.js
+++ main.js
@@ -1,43 +1,50 @@
 const fs = require("fs");
 const bencode = require("bencode");
 const url = require("url");
-const dgram = require("dgram");
 const http = require("http");
+const crypto = require("crypto");
+const hex2ascii = require("hex2ascii");
+const querystring = require("querystring");
+const dgram = require("dgram");
 const client = dgram.createSocket("udp4");
 
 const message = Buffer.from("hello", "utf-8");
 
 fs.readFile("music.torrent", (err, data) => {
-    torrent = bencode.decode(data);
-    tracker = url.parse(torrent.announce.toString('utf8'));
-
-    console.log(torrent.info.filehash.toString('hex'));
-
-    const options = {
-        hostname: tracker.hostname,
-        port: tracker.port,
-        path: '?' + tracker.path + '=' + torrent.info.filehash.toString('hex'),
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': message.length
-        }
-    }
+    const torrent = bencode.decode(data);
+    const tracker = url.parse(torrent.announce.toString('utf8'));
+    const info_hash = crypto.createHash('sha1').update(bencode.encode(torrent.info)).digest();
+    const peer_id = Buffer.from('-NT0001-');
+    console.log(querystring.escape('-NT0001-'));
+    
 
     if (tracker.protocol == "http:") {
         console.log("http protocol");
+
+        const options = {
+            hostname: tracker.hostname,
+            port: tracker.port,
+            path: tracker.path
+            + '?info_hash=' + escape(hex2ascii(info_hash.toString('hex'))) 
+            + '&peer_id=' + escape('-NT0001-' + crypto.randomBytes(12)),
+            method: 'GET'
+        }
+          
         const req = http.request(options, res => {
             console.log(`statusCode: ${res.statusCode}`);
           
             res.on('data', d => {
-                console.log(d);
+                process.stdout.write(d);
             });
         });
           
-        req.on('error', err => {
-            console.error(err);
+        req.on('error', error => {
+            console.error(error);
         });
+          
+        req.end();
     }
+
     else if (torrent.protocol = "udp:") {
         console.log("udp protocol");
         client.send(message, 0, message.length, tracker.port, tracker.hostname, (err, bytes) => {
@@ -52,5 +59,3 @@ fs.readFile("music.torrent", (err, data) => {
         console.log("알 수 없는 프로토콜 (Unknown Protocol");
     }
 });
-
-
