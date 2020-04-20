f95f64e09aeada1c2dca5461a5bc067e60b40bd5
박태원 <producer82@naver.com>
2020-04-20 18:09:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ fs.readFile("music.torrent", (err, data) => {
     const tracker = url.parse(torrent.announce.toString('utf8'));
     const info_hash = crypto.createHash('sha1').update(bencode.encode(torrent.info)).digest();
     const peer_id = Buffer.from('-NT0001-');
-    console.log(querystring.escape('-NT0001-'));
+    var peer;
     
 
     if (tracker.protocol == "http:") {
@@ -26,7 +26,8 @@ fs.readFile("music.torrent", (err, data) => {
             port: tracker.port,
             path: tracker.path
             + '?info_hash=' + escape(hex2ascii(info_hash.toString('hex'))) 
-            + '&peer_id=' + escape('-NT0001-' + crypto.randomBytes(12)),
+            + '&peer_id=' + escape(peer_id + hex2ascii(crypto.randomBytes(12).toString('hex')))
+            + '&port=6881',
             method: 'GET'
         }
           
@@ -35,16 +36,18 @@ fs.readFile("music.torrent", (err, data) => {
           
             res.on('data', d => {
                 process.stdout.write(d);
+                peer = d;
             });
         });
           
         req.on('error', error => {
             console.error(error);
         });
-          
+        
+        console.log(bencode.decode(peer));
+
         req.end();
     }
-
     else if (torrent.protocol = "udp:") {
         console.log("udp protocol");
         client.send(message, 0, message.length, tracker.port, tracker.hostname, (err, bytes) => {
